bb08863788ddec5d4770d0f11d3c328bbf8db29e
zinix758-eng <zinix758@gmail.com>
2026-09-02 11:05:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-const SUPABASE_URL = "https://uopwsaymtomnxfmacsnu.supabase.co";
 
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
 const $=s=>document.querySelector(s);
 let username=localStorage.getItem("vibechat_name")||"";
 let currentRoom="Chill Zone";
